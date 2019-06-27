257b1fa5d805c5fa6fd2fbf211dbfc6650c3f5c9
Jonathan Nilsson <jonathan@voysys.se>
2019-06-27 19:47:16 +0200

    Roll

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    public float torque;
+    public Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("w"))
+        {
+            rb.AddTorque(transform.right * torque);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddTorque(-transform.right * torque);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddTorque(-transform.up * torque);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddTorque(transform.up * torque);
+        }
     }
 }
