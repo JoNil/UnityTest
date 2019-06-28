ab1cda8586733612c2c3bfbdb5ed09ba1021869f
Jonathan Nilsson <jonathan@voysys.se>
2019-06-29 00:17:00 +0200

    Woho

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,36 +4,54 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    public float torque;
-    public Rigidbody rb;
+    public float angle = 0.0f;
+    public float rotationSpeed = 100.0f;
+    public Vector3 cameraOffset = new Vector3(0.0f, 1.5f, -2.0f);
+    public float torque = 200.0f;
+
+    private Rigidbody rb;
+    private GameObject gameCamera;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        gameCamera = GameObject.Find("/Main Camera");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("w"))
+        var cameraLocalPos = Quaternion.AngleAxis(angle, Vector3.up) * cameraOffset;
+
+        if (gameCamera != null)
         {
-            rb.AddTorque(transform.right * torque);
+            gameCamera.transform.position = transform.position + cameraLocalPos;
+            gameCamera.transform.LookAt(transform.position + new Vector3(0.0f, 1.0f, 0.0f));
         }
 
-        if (Input.GetKey("s"))
         {
-            rb.AddTorque(-transform.right * torque);
-        }
+            var torqueAxis = Vector3.Cross(cameraLocalPos, Vector3.up).normalized;
 
-        if (Input.GetKey("d"))
-        {
-            rb.AddTorque(-transform.up * torque);
+            if (Input.GetKey("w"))
+            {
+                rb.AddTorque(torqueAxis * torque);
+            }
+
+            if (Input.GetKey("s"))
+            {
+                rb.AddTorque(-torqueAxis * torque);
+            }
         }
 
         if (Input.GetKey("a"))
         {
-            rb.AddTorque(transform.up * torque);
+            angle -= rotationSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            angle += rotationSpeed * Time.deltaTime;
         }
     }
 }
